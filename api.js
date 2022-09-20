753fe66a059dc8f52fa2c8108efd7e9d126df3ac
ketsarapornHS <114001889+ketsarapornHS@users.noreply.github.com>
2022-09-20 21:45:45 +0700

    Update api.js

--- api.js
+++ api.js
@@ -1,6 +1,6 @@
 //Url to Google Sheets
 //const url = 'https://docs.google.com/spreadsheets/d/[sheet ID]/gviz/tq?';
-const url = 'https://docs.google.com/spreadsheets/d/1hmB8AsaUPsuhDuOPoegFK5zTj3ypjwoEiiS7ulg4zIk/gviz/tq?';
+const url = 'https://docs.google.com/spreadsheets/d/1qqcewCVPu3hTQTlgLBHbPWKJShlVnEiKu4ZdHLctPvc/gviz/tq?';
 const productsContainer = document.querySelector('.products-grid');
     fetch(url)
     .then(res => res.text())
